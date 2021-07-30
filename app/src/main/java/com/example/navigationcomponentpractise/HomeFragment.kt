9275be22746d09d7a7bbc8ce2fc8f7c0d5d625b1
lalit <lalitrajput069@gmail.com>
2021-07-30 11:41:03 +0530

    Added SafeGrade Functionality

--- app/src/main/java/com/example/navigationcomponentpractise/HomeFragment.kt
+++ app/src/main/java/com/example/navigationcomponentpractise/HomeFragment.kt
@@ -5,16 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavDirections
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.navigationcomponentpractise.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+    ): View {
 
+            // Inflate the layout for this fragment using DataBinding
+            binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+            binding.logIn.setOnClickListener {
+                val action = HomeFragmentDirections.actionHomeFragmentToLogInScreen()
+                findNavController().navigate(action)   //with Safe args
+//              findNavController().navigate(R.id.action_homeFragment_to_logInScreen) // without Safe args
+            }
+            return binding.root
+    }
 }
--- app/src/main/java/com/example/navigationcomponentpractise/LogInScreen.kt
+++ app/src/main/java/com/example/navigationcomponentpractise/LogInScreen.kt
@@ -15,4 +15,10 @@ class LogInScreen : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_log_in_screen, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
 }
