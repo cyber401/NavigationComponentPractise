a2538b939501cc9d9a1f130de4200ef4e53be95d
lalit <lalitrajput069@gmail.com>
2021-08-03 13:16:12 +0530

    Added ToolBar in MainActivity.kt and Back Arrow Button

--- app/src/main/java/com/example/navigationcomponentpractise/MainActivity.kt
+++ app/src/main/java/com/example/navigationcomponentpractise/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.navigationcomponentpractise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.navigationcomponentpractise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var navController:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
+
+
+        }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
